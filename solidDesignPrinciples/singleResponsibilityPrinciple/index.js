135224053167a449fc6ad1ff98cc23018657dbf0
Samuel Njenga <snjenga704@gmail.com>
2020-09-15 21:02:40 +0300

    Created solidDesignPrinciples, inside it added singleResponsibilityPrinciple directory and inside it added script.js and index.js which uses singleResponsibilityPrinciple in it

---
+++ solidDesignPrinciples/singleResponsibilityPrinciple/index.js
@@ -0,0 +1,20 @@
+import logMessage from './logger'
+class CalorieTracker {
+    constructor(maxCalories) {
+        this.maxCalories = maxCalories
+        this.currentCalories = 0
+    }
+    trackCalories(calorieCount) {
+        this.currentCalories += calorieCount
+        if (this.currentCalories > this.maxCalories) {
+            logMessage('Max calories exceeded')
+        }
+    }
+    // logCalorieSurplus() {
+    //     console.log('Max calories exceeded');
+    // }
+}
+const calorieTracker = new CalorieTracker(2000)
+calorieTracker.trackCalories(500)
+calorieTracker.trackCalories(1000)
+calorieTracker.trackCalories(700)
---
+++ solidDesignPrinciples/singleResponsibilityPrinciple/script.js
@@ -0,0 +1,19 @@
+class CalorieTracker {
+    constructor(maxCalories) {
+        this.maxCalories = maxCalories
+        this.currentCalories = 0
+    }
+    trackCalories(calorieCount) {
+        this.currentCalories += calorieCount
+        if (this.currentCalories > this.maxCalories) {
+            this.logCalorieSurplus()
+        }
+    }
+    logCalorieSurplus() {
+        console.log('Max calories exceeded');
+    }
+}
+const calorieTracker = new CalorieTracker(2000)
+calorieTracker.trackCalories(500)
+calorieTracker.trackCalories(1000)
+calorieTracker.trackCalories(700)
